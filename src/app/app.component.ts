8fc1931f69bfc09d91f79902fca8e385e8460dd2
gallego93 <gallego9351@gmail.com>
2025-01-09 15:02:32 -0500

    Firts commit: inicializando proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
+import { HeroComponent } from "./components/hero/hero.component";
+import { FeatureComponent } from "./components/feature/feature.component";
+import { CtaComponent } from "./components/cta/cta.component";
+import { TestimonialComponent } from "./components/testimonial/testimonial.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { FooterComponent } from "./components/footer/footer.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule, HomeComponent, HeroComponent, FeatureComponent, CtaComponent, TestimonialComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-spa';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { FeatureComponent } from './components/feature/feature.component';
+import { CtaComponent } from './components/cta/cta.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'hero', component: HeroComponent },
+    { path: 'feature', component: FeatureComponent },
+    { path: 'cta', component: CtaComponent },
+    { path: 'testimonial', component: TestimonialComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'footer', component: FooterComponent },
+    { path: '404', component: PageNotFoundComponent },
+    { path: '**', redirectTo: '404', pathMatch: 'full' },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule { }
